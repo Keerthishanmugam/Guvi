36f2c8ee0f036549ae3311e78a88c7fa44b9276d
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-08-01 13:59:41 +0530

    Create MaximumSum.java

---
+++ MaximumSum.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+public class MaximumSum {
+public static void main(String args[])throws IOException
+{
+	int number,i,sum=0;
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("Enter the number of elements in the array");
+	number=Integer.parseInt(br.readLine());
+	int[] values=new int[number];
+	for(i=0;i<number;i++)
+	{
+	 values[i]=Integer.parseInt(br.readLine());
+	}
+	Arrays.sort(values);
+	for(i=number;i>number-3;i--)
+	{
+	sum=sum+values[i-1];
+	}
+	System.out.println(sum);
+}
+}
